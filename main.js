2f961761db1aeba3f4d2a2a150feb19f9e016ab5
Liam Harrison <liam.harrison@mymail.champlain.edu>
2023-11-06 14:30:43 -0500

    Set up the main structure of the program

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 (() =>
     {
+        const imageFilePath = "images/";
+
+
         let youtubeLeftControls, youtubePlayer;
         let currentVideo = ""; //stores the current video
         chrome.runtime.onmessage.addListener((obj, sender, response) =>
@@ -12,5 +15,33 @@
                 newVideoLoaded();
             }
         })
+
+        //NOTE: The purpose of this function is to get all YouTube thumbnails on the page
+        function getThumbnails()
+        {
+            //TODO: Implement
+        }
+
+        //NOTE: The purpose of this function is to apply the thumbnail images to the thumbnails on YouTube.com
+        function applyThumbnails()
+        {
+            //TODO: Implement
+        }
+
+        //NOTE: The purpose of this function is to get a random image to display
+        function getRandomImage()
+        {
+            //NOTE: percent is even across the board for any given image to be chosen
+            //TODO: Implement
+        }
+
+        //NOTE: The purpose of this function is to randomly determine whether or not to flip the image or not
+        function getImageState()
+        {
+            //NOTE: percent to flip is 50%
+            //TODO: Implement
+
+        }
+
     }
 )();
