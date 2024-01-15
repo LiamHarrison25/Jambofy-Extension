97339e7cd8eff766b7ea13aaace0f14add677a8d
SuppliedOrange <blackandantiqual@gmail.com>
2024-01-16 02:17:52 +0530

    Added shorts support
    + Flipped all images to originate from the right
    + Made getThumbnails() get shorts thumbnails and replace them as well. It also uses a new overlay format.
    + Random Images now has a parameter to ignore a few images in particular.

--- main.js
+++ main.js
@@ -2,18 +2,19 @@
     {
         const imageFilePath = "assets/images/";
         const numImages = 103;
-        const flipRandomPercent = 3; //NOTE: the number represents how many numbers to randomly choose. bigger = less likely, smaller = more likely.
+        const flipRandomPercent = 2; //NOTE: the number represents how many numbers to randomly choose. bigger = less likely, smaller = more likely.
         var isEnabled = true;
 
         //NOTE: The purpose of this function is to get all YouTube thumbnails on the page
         function getThumbnails()
         {
             const thumbnailQuery = "ytd-thumbnail:not(.ytd-video-preview, .ytd-rich-grid-slim-media) a > yt-image > img.yt-core-image:only-child:not(.yt-core-attributed-string__image-element),.ytp-videowall-still-image:not([style*='extension:'])";
+            // This query selects all yt thumbnails that are regular videos
 
             const thumbnail = document.querySelectorAll(thumbnailQuery);
 
             thumbnail.forEach((image) =>
-                {
+                {   
                     let counter = Math.random() > 0.001 ? 1 : 20;
                     let i = 0;
                     for(i = 0; i < counter; i++)
@@ -21,10 +22,33 @@
                         const index = getRandomImage();
                         let flip = getImageState();
                         let url = getImageURL(index);
-                        applyThumbnails(image, url, flip);
+                        applyThumbnails(image, url, flip, shorts = false);
                     }
                 }
             )
+
+            const shortsThumbnailQuery = "ytd-thumbnail:not(.ytd-video-preview, .ytd-rich-grid-media) a > yt-image > img.yt-core-image:only-child:not(.yt-core-attributed-string__image-element),.ytp-videowall-still-image:not([style*='extension:'])";
+            // This query selects all yt thumbnails that are shorts
+
+            const shortsThumbail = document.querySelectorAll(shortsThumbnailQuery);
+
+            // Images that cut off / originate at the top will look bad. These are those images. It's not necessary but more of a QOL fix.
+            let images_cut_off_at_the_top = [12, 23, 93, 6];
+
+            shortsThumbail.forEach((image) =>
+            {   
+                let counter = Math.random() > 0.001 ? 1 : 20;
+                let i = 0;
+                for(i = 0; i < counter; i++)
+                {
+                    const index = getRandomImage( ignore = images_cut_off_at_the_top );
+                    let flip = getImageState();
+                    let url = getImageURL(index);
+                    applyThumbnails(image, url, flip, shorts = true);
+                }
+            }
+        )
+
         }
 
         //NOTE: The purpose of this function is to return the url of an image
@@ -34,18 +58,36 @@
         }
 
         //NOTE: The purpose of this function is to apply the thumbnail images to the thumbnails on YouTube.com
-        function applyThumbnails(image, imageUrl, flip = false)
+        function applyThumbnails(image, imageUrl, flip = false, shorts = false)
         {
             if (image.nodeName == "IMG")
             {
                 const overlay = document.createElement("img");
-                overlay.src = imageUrl;
-                overlay.style.position = "absolute";
-                overlay.style.top = "0";
-                overlay.style.left = "0";
-                overlay.style.width = "100%";
-                overlay.style.height = "100%";
-                overlay.style.zIndex = "0";
+
+                if (shorts) {
+
+                    overlay.src = imageUrl;
+                    overlay.style.position = "absolute";
+                    overlay.style.top = "37.5%";
+                    if (flip) overlay.style.left = "0";
+                    else overlay.style.right = "0";
+                    overlay.style.width = "175%";
+                    overlay.style.height = "65.625%";
+                    overlay.style.zIndex = "0";
+
+                }
+                else {
+
+                    overlay.src = imageUrl;
+                    overlay.style.position = "absolute";
+                    overlay.style.top = "0";
+                    overlay.style.left = "0";
+                    overlay.style.right = "0";
+                    overlay.style.width = "100%";
+                    overlay.style.height = "100%";
+                    overlay.style.zIndex = "0";
+
+                }                
                 if(flip)
                 {
                     overlay.style.transform = "scaleX(-1)"; //flips the image
@@ -65,13 +107,17 @@
             return Math.floor(Math.random() * max);
         }
 
-        //NOTE: The purpose of this function is to get a random image to display
-        function getRandomImage()
+        //NOTE: The purpose of this function is to get a random image to display. It ignores all numbers in `ignore` (array)
+        function getRandomImage( ignore = null )
         {
             //NOTE: percent is even across the board for any given image to be chosen
 
             let random = 0;
-            random = getRandomInt(numImages + 1); //NOTE: +1 is because max is not inclusive
+            while (random == 0 || ignore?.includes(random)) // If random isn't initialized or if it's a number that's included in ignore, get another random number.
+            {
+                random = getRandomInt(numImages + 1); //NOTE: +1 is because max is not inclusive
+            }
+            
             return random;
         }
 
