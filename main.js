501d4a5998053066d36918ea03d58844615c12e9
SuppliedOrange <blackandantiqual@gmail.com>
2024-01-16 06:10:23 +0530

    Major Update, Playlists added
    + Playlists now have kitties
    + Code has been revamped a little
    + Music albums will NOT have Jambo. They are FORBIDDEN from Jambo.
    + Basically every thumbnail on youtube should work now.

--- main.js
+++ main.js
@@ -6,48 +6,71 @@
         var isEnabled = true;
 
         //NOTE: The purpose of this function is to get all YouTube thumbnails on the page
+
         function getThumbnails()
         {
-            const thumbnailQuery = "ytd-thumbnail:not(.ytd-video-preview, .ytd-rich-grid-slim-media) a > yt-image > img.yt-core-image:only-child:not(.yt-core-attributed-string__image-element),.ytp-videowall-still-image:not([style*='extension:'])";
-            // This query selects all yt thumbnails that are regular videos
+            let thumbnails = [];
 
-            const thumbnail = document.querySelectorAll(thumbnailQuery);
+            function pullThumbnail(query, type) {
 
-            thumbnail.forEach((image) =>
-                {   
-                    let counter = Math.random() > 0.001 ? 1 : 20;
-                    let i = 0;
-                    for(i = 0; i < counter; i++)
-                    {
-                        const index = getRandomImage();
-                        let flip = getImageState();
-                        let url = getImageURL(index);
-                        applyThumbnails(image, url, flip, shorts = false);
-                    }
-                }
-            )
+                const thumbnails_recieved = document.querySelectorAll(query);
 
-            const shortsThumbnailQuery = "ytd-thumbnail:not(.ytd-video-preview, .ytd-rich-grid-media) a > yt-image > img.yt-core-image:only-child:not(.yt-core-attributed-string__image-element),.ytp-videowall-still-image:not([style*='extension:'])";
-            // This query selects all yt thumbnails that are shorts
+                thumbnails_recieved.forEach( thumbnail => {
 
-            const shortsThumbail = document.querySelectorAll(shortsThumbnailQuery);
+                    thumbnails.push({
+                        image: thumbnail,
+                        type: type,
+                        index: null, flip: null, url: null
+                    });
+
+                })
+            }
+
+            let default_ignorables = [".ytd-compact-station-renderer", ".ytd-station-renderer"]
+
+            pullThumbnail(
+                query = `ytd-thumbnail:not(.ytd-video-preview, .ytd-rich-grid-slim-media, .ytd-reel-item-renderer, ${default_ignorables.join()} ) a > yt-image > img.yt-core-image:only-child:not(.yt-core-attributed-string__image-element),.ytp-videowall-still-image:not([style*='extension:'])`,
+                type = "standard_video"
+            ) // This query selects all yt thumbnails that are regular videos. (rich-grid-media, grid-video-renderer, compact-video-renderer)
+
+            pullThumbnail(
+                query = `ytd-thumbnail:not(.ytd-video-preview, .ytd-rich-grid-media, .ytd-video-renderer, .ytd-grid-video-renderer, .ytd-compact-video-renderer, .ytd-playlist-panel-video-renderer, ${default_ignorables.join()} ) a > yt-image > img.yt-core-image:only-child:not(.yt-core-attributed-string__image-element),.ytp-videowall-still-image:not([style*='extension:'])`,
+                type = "shorts_video"
+            ) // This query selects all yt thumbnails that are shorts (rich-grid-slim-media). Note that this also works for reel-items.
+
+            pullThumbnail(
+                query =  `ytd-playlist-video-thumbnail-renderer:not(${default_ignorables.join()}) yt-image > img.yt-core-image:only-child:not(.yt-core-attributed-string__image-element),.ytp-videowall-still-image:not([style*='extension:'])`,
+                type = "standard_video"
+            ) // This query selects all yt playlist thumbnails, treats them as standard thumbnails
 
             // Images that cut off / originate at the top will look bad. These are those images. It's not necessary but more of a QOL fix.
             let images_cut_off_at_the_top = [12, 23, 93, 6];
 
-            shortsThumbail.forEach((image) =>
-            {   
-                let counter = Math.random() > 0.001 ? 1 : 20;
-                let i = 0;
-                for(i = 0; i < counter; i++)
-                {
-                    const index = getRandomImage( ignore = images_cut_off_at_the_top );
-                    let flip = getImageState();
-                    let url = getImageURL(index);
-                    applyThumbnails(image, url, flip, shorts = true);
+            thumbnails.forEach((thumbnail) =>
+                {   
+                    let counter = Math.random() > 0.001 ? 1 : 20;
+                    let i = 0;
+                    for(i = 0; i < counter; i++)
+                    {        
+
+                        // Sometimes standard videos can be standardized shorts thumbnails ( shorts in the dimensions of a standard video )
+                        if (thumbnail.type == "standard_video" && thumbnail.image.parentNode.parentNode.children[1]?.children[0]?.attributes[1]?.value == "SHORTS")
+                        {
+                            thumbnail.type = "standardized_shorts_video"
+                            console.log(thumbnail.image)
+                        } 
+
+                        if (thumbnail.type != "standard_video") thumbnail.index = getRandomImage(ignore = images_cut_off_at_the_top); // Only standard videos don't need to disinclude images that cut off at the top.
+                        else thumbnail.index = getRandomImage()
+                        
+                        thumbnail.flip = getImageState();
+                        thumbnail.url = getImageURL(thumbnail.index);
+
+                        applyThumbnail(thumbnail);
+                        
+                    }
                 }
-            }
-        )
+            )
 
         }
 
@@ -58,46 +81,63 @@
         }
 
         //NOTE: The purpose of this function is to apply the thumbnail images to the thumbnails on YouTube.com
-        function applyThumbnails(image, imageUrl, flip = false, shorts = false)
+        function applyThumbnail(thumbnail)
         {
-            if (image.nodeName == "IMG")
+
+            if (thumbnail.image.nodeName == "IMG")
             {
                 const overlay = document.createElement("img");
 
-                if (shorts) {
+                switch(thumbnail.type) {
 
-                    overlay.src = imageUrl;
-                    overlay.style.position = "absolute";
-                    overlay.style.top = "37.5%";
-                    if (flip) overlay.style.left = "0";
-                    else overlay.style.right = "0";
-                    overlay.style.width = "175%";
-                    overlay.style.height = "65.625%";
-                    overlay.style.zIndex = "0";
+                    case "shorts_video":
+                        overlay.src = thumbnail.url;
+                        overlay.style.position = "absolute";
+                        overlay.style.top = "37.5%";
+                        if (thumbnail.flip) overlay.style.left = "0";
+                        else overlay.style.right = "0";
+                        overlay.style.width = "175%";
+                        overlay.style.height = "65.625%";
+                        overlay.style.zIndex = "0";
+                        break;
+
+                    case "standard_video":
+                        overlay.src = thumbnail.url;
+                        overlay.style.position = "absolute";
+                        overlay.style.top = "0";
+                        overlay.style.left = "0";
+                        overlay.style.right = "0";
+                        overlay.style.width = "100%";
+                        overlay.style.height = "100%";
+                        overlay.style.zIndex = "0";
+                        break;
+                    
+                    case "standardized_shorts_video":
+                        overlay.src = thumbnail.url;
+                        overlay.style.position = "absolute";
+                        overlay.style.top = "65%";
+                        if (thumbnail.flip) overlay.style.left = "34%";
+                        else overlay.style.right = "34%";
+                        overlay.style.width = "48%";
+                        overlay.style.height = "35%";
+                        overlay.style.zIndex = "0";
+                        break;
+                    
 
                 }
-                else {
 
-                    overlay.src = imageUrl;
-                    overlay.style.position = "absolute";
-                    overlay.style.top = "0";
-                    overlay.style.left = "0";
-                    overlay.style.right = "0";
-                    overlay.style.width = "100%";
-                    overlay.style.height = "100%";
-                    overlay.style.zIndex = "0";
-
-                }                
-                if(flip)
+                if(thumbnail.flip)
                 {
                     overlay.style.transform = "scaleX(-1)"; //flips the image
                 }
-                image.style.position = "relative";
-                image.parentElement.appendChild(overlay);
+
+                thumbnail.image.style.position = "relative";
+                thumbnail.image.parentElement.appendChild(overlay);
+
             }
-            else if (image.nodeName == "DIV")
+            else if (thumbnail.image.nodeName == "DIV")
             {
-                image.style.backgroundImage = `url("${imageUrl}"), ` + image.style.backgroundImage;
+                thumbnail.image.style.backgroundImage = `url("${thumbnail.url}"), ` + thumbnail.image.style.backgroundImage;
             }
         }
 
