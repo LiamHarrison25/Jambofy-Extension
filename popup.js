f1bfeba7a65fdad3d763fb1220b6638c92b69aba
Liam Harrison <liam.harrison@mymail.champlain.edu>
2024-08-19 16:49:59 -0400

    Added new images and worked on the enable button for the popup.

--- popup.js
+++ popup.js
@@ -1,23 +1,33 @@
 
-//SECTION: Opacity Slider
-const slider = document.getElementById("myRange");
-const output = document.getElementById("demo");
-output.innerHTML = slider.value;
+//Elements:
+const enableButton = document.getElementById("enable");
+const disableButton = document.getElementById("disable");
 
-slider.oninput = (e) => {
-    output.innerHTML = e.target.value;
-}
+
+//SECTION: Opacity Slider
+// const slider = document.getElementById("myRange");
+// const output = document.getElementById("demo");
+// output.innerHTML = slider.value;
+//
+// slider.oninput = (e) => {
+//     output.innerHTML = e.target.value;
+// }
 
 //SECTION: Toggle Button
 
 var imagesEnabled = true;
 
+enableButton.onclick = ToggleImages;
+
+disableButton.onclick = ToggleImages;
+
 function ToggleImages(event)
 {
+    console.log("toggles the images");
     imagesEnabled = !imagesEnabled;
 }
 
-document.getElementById("ToggleButton").addEventListener('click', ToggleImages);
+//document.getElementById("ToggleButton").addEventListener('click', ToggleImages);
 
 
 //Outdated:
